bb4bf872fa25b822ea080e82bba69c57dd6609ef
Yuna <yuna.bonnifet@studioblackflag.com>
2024-06-26 23:01:55 +0200

    Add item, player set up, and make network work !

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -1,12 +1,29 @@
 using Mirror;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomNetworkManager : NetworkManager
 {
+	[Header("CUSTOM")]
+	[SerializeField] List<Transform> spawnList;
+
 	public override void OnServerAddPlayer(NetworkConnectionToClient conn)
 	{
 		base.OnServerAddPlayer(conn);
+		
+		//Transform spawn = null;
 
-		Debug.Log("A player joined !");
+		//if (spawnList.Count > 0)
+		//{
+		//	if (numPlayers <= spawnList.Count)
+		//		spawn = spawnList[numPlayers - 1];
+		//	else
+		//		spawn = spawnList[0];
+		//}
+
+		//GameObject player = spawn ? Instantiate(playerPrefab, spawn.position, spawn.rotation) : Instantiate(playerPrefab);
+
+		//player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";//Change "Player(clone)" => to connectionId (for debugging)
+		//NetworkServer.AddPlayerForConnection(conn, player);
 	}
 }
