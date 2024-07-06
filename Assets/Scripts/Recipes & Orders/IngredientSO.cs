b6884c9b9854771e705237614f6d372313e270f0
Yuna <yuna.bonnifet@studioblackflag.com>
2024-07-06 12:57:28 +0200

    Move Files

--- Assets/Scripts/Recipes & Orders/IngredientSO.cs
+++ Assets/Scripts/Recipes & Orders/IngredientSO.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
 public class IngredientSO : ScriptableObject
 {
-	[SerializeField] string name;
+	public string name;
 	public Sprite sprite;
 }
